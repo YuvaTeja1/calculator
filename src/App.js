e11ae41d942e8590cb8b33972cf0d29678054d9c
YUVA TEJA <99013669+YuvaTeja1@users.noreply.github.com>
2022-08-28 15:44:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
+const App = () => {
+  const [input,setInput] = useState("");
+  const [result,setResult] = useState(0);
+  const handler = e =>{
+    setInput(e.target.value);
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center className='center'>
+        <h1>Calculator</h1><hr/>
+        <input type='text' name='input' value={input} onChange={handler}/> <br/>
+        <button onClick={() => setResult(eval(input))}>Calculate</button> <br/>
+        <h4>Result is : {result}</h4>
+        <button onClick={() => setInput(input+'1')}>1</button>
+        <button onClick={() => setInput(input+'2')}>2</button>
+        <button onClick={() => setInput(input+'3')}>3</button><br/>
+        <button onClick={() => setInput(input+'4')}>4</button>
+        <button onClick={() => setInput(input+'5')}>5</button>
+        <button onClick={() => setInput(input+'6')}>6</button><br/>
+        <button onClick={() => setInput(input+'7')}>7</button>
+        <button onClick={() => setInput(input+'8')}>8</button>
+        <button onClick={() => setInput(input+'9')}>9</button><br/>
+        <button onClick={() => setInput(input+'+')}>+</button>
+        <button onClick={() => setInput(input+'-')}>-</button>
+        <button onClick={() => setInput(input+'*')}>*</button>
+        <button onClick={() => setInput(input+'/')}>/</button><br/>
+        <button onClick={() => setInput('')}>clear</button><br />
+      </center><br />
+
     </div>
-  );
+  )
 }
 
 export default App;
